9e8d39e1193d046984783bbcf76ae0cf0c68ba02
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-03-20 14:34:33 +0330

    update income & expense

--- expense-tracker/src/components/ExpenseApp.jsx
+++ expense-tracker/src/components/ExpenseApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import OverViewComponent from "./OverViewComponent";
 import TransActionComponent from "./TransActionComponent";
 
@@ -10,8 +10,20 @@ const ExpenseApp = () => {
   const addTransaction = (formValues) => {
     console.log(formValues);
     const data = { ...formValues, id: Date.now() };
-    setTransaction([...transaction, data])
+    setTransaction([...transaction, data]);
   };
+
+  useEffect(() => {
+    let exp = 0;
+    let inc = 0;
+    transaction.forEach((t) => {
+      t.type === "expense"
+        ? (exp = exp + parseFloat(t.amount))
+        : (inc = inc + parseFloat(t.amount));
+    });
+    setExpense(exp);
+    setIncome(inc);
+  }, [transaction]);
 
   return (
     <section className="container">
