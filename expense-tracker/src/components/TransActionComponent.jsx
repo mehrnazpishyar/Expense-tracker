ac51483a674ffb1d740cdec4353c4e8de11e16b9
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-03-21 18:20:36 +0330

    add search option to transactions

--- expense-tracker/src/components/TransActionComponent.jsx
+++ expense-tracker/src/components/TransActionComponent.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TransActionComponent = ({ transaction }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredTnx, setFilteredTnx] = useState(transaction);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilteredTnx(transaction);
+      return;
+    }
+    const filtered = transaction.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTnx(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transaction]);
+
+  if (!transaction.length) return <h3 style={{marginTop:"20px"}}>add some transaction</h3>
+
   return (
     <section>
-      {transaction.length &&
-        transaction.map((t) => {
-          return (
-            <div className="transaction" key={t.id} style={{borderRight: t.type === "expense" && "4px solid red"}}>
-              <span>{t.desc}</span>
-              <span>$ {t.amount}</span>
-            </div>
-          );
-        })}
+      <input className="search" type="text" value={searchItem} onChange={changeHandler} placeholder="search..."/>
+      {filteredTnx.length
+        ? filteredTnx.map((t) => {
+            return (
+              <div
+                className="transaction"
+                key={t.id}
+                style={{ borderRight: t.type === "expense" && "4px solid red" }}
+              >
+                <span>{t.desc}</span>
+                <span>$ {t.amount}</span>
+              </div>
+            );
+          })
+        : "no item matchs!"}
     </section>
   );
 };
